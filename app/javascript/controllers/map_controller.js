d8fd0c296e16e5e75524b07f7fa36d5709d9dfbd
Millene Prado <millene_vprado@hotmail.com>
2022-11-23 10:51:34 -0300

    finished lecture

---
+++ app/javascript/controllers/map_controller.js
@@ -0,0 +1,52 @@
+import { Controller } from "@hotwired/stimulus"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
+
+// Connects to data-controller="map"
+export default class extends Controller {
+  static values = {
+    apiKey: String,
+    markers: Array
+  }
+
+  connect() {
+    mapboxgl.accessToken = this.apiKeyValue;
+
+    this.map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12'
+    });
+
+    this.#addMarkersToMap();
+    this.#fitMapToMarkers();
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
+  }
+
+  #addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window)
+
+      const customMarker = document.createElement("div")
+      customMarker.style.width = "30px"
+      customMarker.style.height = "30px"
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`
+      customMarker.style.backgroundSize = 'contain'
+      customMarker.style.borderRadius = '50%'
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+    })
+  }
+
+  #fitMapToMarkers() {
+    const bounds = new mapboxgl.LngLatBounds()
+    this.markersValue.forEach((marker) => {
+      bounds.extend([marker.lng, marker.lat])
+    })
+    this.map.fitBounds(bounds, {
+      padding: 50,
+      duration: 0
+    })
+  }
+}
